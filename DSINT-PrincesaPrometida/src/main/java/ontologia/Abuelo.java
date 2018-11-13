1b9423655258c3ffe6e147fb1a5318095b40dadf
norbr9 <45005534+norbr9@users.noreply.github.com>
2018-11-13 15:18:20 +0100

    Update Abuelo.java

--- DSINT-PrincesaPrometida/src/main/java/ontologia/Abuelo.java
+++ DSINT-PrincesaPrometida/src/main/java/ontologia/Abuelo.java
@@ -2,11 +2,17 @@ package ontologia;
 
 public class Abuelo extends Personaje {
 
-	//TODO añadir relacion familiar_de
+	//TODO aÃ±adir relacion familiar_de
 
 	public Abuelo(EstadoCivil estadoCivil, EstadoSalud salud, String nombre, Origen origen, boolean cuento,
 			Sexo sexo, TipoImportancia importancia, Lugar ubicacion) {
 		super(estadoCivil, salud, nombre, origen,cuento, sexo, importancia, ubicacion);
 		
 	}
+	
+	
+	
+	
+	/// comentario norberto
+	
 }
