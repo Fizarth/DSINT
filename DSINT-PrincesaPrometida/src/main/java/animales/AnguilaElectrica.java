b0424703c7753e4f7de57c4135650c2b185f7ba4
Usuario <Usuario@172.22.73.52>
2018-11-14 11:46:47 +0100

    Creación de los animales

--- DSINT-PrincesaPrometida/src/main/java/animales/AnguilaElectrica.java
+++ DSINT-PrincesaPrometida/src/main/java/animales/AnguilaElectrica.java
@@ -1,8 +1,5 @@
 package animales;
-import lugares.*;
 
 public class AnguilaElectrica extends Animal {
-public AnguilaElectrica() {
-	super.ubicacionActual = new Mar();
-}
+
 }
--- DSINT-PrincesaPrometida/src/main/java/animales/Animal.java
+++ DSINT-PrincesaPrometida/src/main/java/animales/Animal.java
@@ -3,7 +3,19 @@ package animales;
 import lugares.*;
 
 public abstract class Animal {
-	
+
 	private Lugar ubicacionActual;
+
+	public Animal() {
+		this.ubicacionActual = null;
+	}
+
+	public Lugar getUbicacionActual() {
+		return ubicacionActual;
+	}
+
+	public void setUbicacionActual(Lugar ubicacionActual) {
+		this.ubicacionActual = ubicacionActual;
+	}
 
 }
