690bcdb0e550190c07713eda59df210b17a88c54
Usuario <Usuario@DESKTOP-8UG5GPE>
2018-11-17 11:15:19 +0100

    Añadidas relaciones que faltaban

---
+++ DSINT-PrincesaPrometida/src/main/java/relaciones/QuiereA.java
@@ -0,0 +1,11 @@
+package relaciones;
+
+import personajes.*;
+
+public class QuiereA extends Relacion {
+
+	public QuiereA(String nombre, Personaje afectado) {
+		super("quiere a", afectado);
+	}
+
+}
