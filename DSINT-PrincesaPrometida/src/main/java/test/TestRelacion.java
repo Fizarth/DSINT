5ea70605abcd049fbf933a561198012bf2a47bae
fires <fires@DESKTOP-IKH9P6T>
2018-11-17 20:04:25 +0100

    asdsafzsfasf

---
+++ DSINT-PrincesaPrometida/src/main/java/test/TestRelacion.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import lugares.*;
+import personajes.*;
+
+public class TestRelacion {
+
+	public static void main(String[] args) {
+
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+		System.out.println(kContainer.verify().getMessages().toString());
+
+		KieSession kSession = kContainer.newKieSession("ksession-rules");
+		
+		Lugar hab = new HabitacionNino();
+		
+		kSession.insert(hab);	
+		
+		Personaje a = new Abuelo(hab);
+		Personaje m = new Madre(hab);
+		Personaje n = new Nieto(hab);
+		
+		kSession.insert(a);
+		kSession.insert(m);
+		kSession.insert(n);
+
+		kSession.fireAllRules();
+	}
+
+}
