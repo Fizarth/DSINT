651be592f6f78235f9e3e0cb11285640edda069e
Usuario <Usuario@172.22.79.50>
2018-11-19 13:22:20 +0100

    Pruebas

--- DSINT-PrincesaPrometida/src/main/java/test/TestActo1.java
+++ DSINT-PrincesaPrometida/src/main/java/test/TestActo1.java
@@ -4,6 +4,9 @@ import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
+import utilidades.Acto;
+import utilidades.ConsultaQue;
+
 public class TestActo1 {
 
 	public static void main(String[] args) {
@@ -13,11 +16,17 @@ public class TestActo1 {
 		//System.out.println(kContainer.verify().getMessages().toString());
 
 		KieSession kSession = kContainer.newKieSession("ksession-rules");
-		
+		ConsultaQue q= new ConsultaQue(Acto.Acto1);
+		kSession.insert(q);
 		kSession.fireAllRules();
+		kSession.getAgenda().getAgendaGroup(q.getActo().getActo()).setFocus();
+		kSession.fireAllRules();
+		
+		/*kSession.fireAllRules();
 		kSession.getAgenda().getAgendaGroup("Acto0").setFocus();
 		kSession.fireAllRules();
 		kSession.getAgenda().getAgendaGroup("Acto1").setFocus();
 		kSession.fireAllRules();
+		*/
 	}
 }
