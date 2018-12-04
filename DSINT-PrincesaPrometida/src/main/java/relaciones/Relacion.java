53c520eb65084ddd9c2d0a3054bdb47d33fd19f3
fires <fires@172.22.78.131>
2018-12-04 16:15:59 +0100

    jhjjhjh

--- DSINT-PrincesaPrometida/src/main/java/relaciones/Relacion.java
+++ DSINT-PrincesaPrometida/src/main/java/relaciones/Relacion.java
@@ -6,39 +6,51 @@ import java.util.List;
 import personajes.*;
 
 public abstract class Relacion {
-	
+
 	private Personaje afectado;
 	private final String nombre;
-	
-	
+
 	public Relacion(String nombre, Personaje afectado) {
 		this.nombre = nombre;
-		this.afectado = afectado;	
+		this.afectado = afectado;
 	}
-		
-	
+
 	/***
 	 * devuelve la lista de relaciones que tiene p1 con p2, siendo p2 el afectado
+	 * 
 	 * @param personaje uno
 	 * @param personaje afectado
 	 * @return Lista con las relaciones que tiene p1 con p2.
 	 */
-	public static List<Relacion> getRelaciones2personajes(Personaje p1, Personaje p2){
+	public static List<Relacion> getRelaciones2personajes(Personaje p1, Personaje p2) {
 		ArrayList<Relacion> relaciones = new ArrayList<>();
 		ArrayList<Relacion> relacionesP1 = p1.getRelacionesPersonaje();
-		
-		
+
 		for (Relacion relacionp1 : relacionesP1) {
-			if (relacionp1.afectado == p2) relaciones.add(relacionp1);
+			if (relacionp1.afectado == p2)
+				relaciones.add(relacionp1);
 		}
-		
+
 		return relaciones;
-		
+
 	}
 
 	@Override
 	public String toString() {
-		return  nombre + " " + afectado.getNombre();
+		return nombre + " " + afectado.getNombre();
 	}
-		
+
+	public static boolean notExistsRelacion(Personaje p1, Personaje p2) {
+		ArrayList<Relacion> relaciones = new ArrayList<>();
+		ArrayList<Relacion> relacionesP1 = p1.getRelacionesPersonaje();
+
+		for (Relacion relacionp1 : relacionesP1) {
+			if (relacionp1.afectado == p2)
+				return false;
+		}
+
+		return true;
+
+	}
+
 }
