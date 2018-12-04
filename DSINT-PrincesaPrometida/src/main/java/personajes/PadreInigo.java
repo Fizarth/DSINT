72a1bc373cd2324364fedb5fa4f977217e3e54a5
MariSolFium <45006481+MariSolFium@users.noreply.github.com>
2018-12-04 13:07:00 +0100

    no me deja hacer pull por commit

--- DSINT-PrincesaPrometida/src/main/java/personajes/PadreInigo.java
+++ DSINT-PrincesaPrometida/src/main/java/personajes/PadreInigo.java
@@ -7,7 +7,7 @@ import utilidades.*;
 public class PadreInigo extends PersonajeCuento{
 	
 	public PadreInigo(Lugar ubicacionAct, Profesion profesion) {
-		super("Domingo Montoya", Origen.ESPANA, Sexo.HOMBRE, EstadoCivil.INDEFINIDO, EstadoSalud.VIVO,  
+		super("Domingo Montoya", Origen.ESPANA, Sexo.HOMBRE, EstadoCivil.INDEFINIDO, EstadoSalud.MUERTO,  
 				TipoImportancia.SECUNDARIO,ubicacionAct, profesion, Motivacion.DESCONOCIDA);
 		
 	}
