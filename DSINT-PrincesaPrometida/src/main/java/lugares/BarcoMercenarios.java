e3f54b4d30a5d049ca62dbc70132d9dd104ec947
MariSolFium <45006481+MariSolFium@users.noreply.github.com>
2018-12-05 19:44:03 +0100

    algunos cambios pequeños y añadidos barcos

---
+++ DSINT-PrincesaPrometida/src/main/java/lugares/BarcoMercenarios.java
@@ -0,0 +1,9 @@
+package lugares;
+
+public class BarcoMercenarios extends Lugar {
+
+	public BarcoMercenarios(String nom) {
+		super("el barco de los mercenarios");
+	}
+
+}
---
+++ DSINT-PrincesaPrometida/src/main/java/lugares/BarcoPirata.java
@@ -0,0 +1,9 @@
+package lugares;
+
+public class BarcoPirata extends Lugar {
+
+	public BarcoPirata(String nom) {
+		super("el barco del pirata");
+	}
+
+}
