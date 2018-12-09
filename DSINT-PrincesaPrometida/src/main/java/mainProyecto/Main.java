f479bb099046dfc53236f9a496cab512ba27d597
MariSolFium <45006481+MariSolFium@users.noreply.github.com>
2018-12-09 12:02:36 +0100

    empezado a tocar el acto2

--- DSINT-PrincesaPrometida/src/main/java/mainProyecto/Main.java
+++ DSINT-PrincesaPrometida/src/main/java/mainProyecto/Main.java
@@ -25,7 +25,7 @@ public class Main {
 		
 		//String filePath = "C:\\Users\\fires\\Desktop\\Nueva\\que.txt";
 
-		String filePath = "C:\\Users\\maris\\Desktop\\pruebasDsint\\pruebaActo1.txt";
+		String filePath = "C:\\Users\\maris\\Desktop\\pruebasDsint\\prueba.txt";
 		Archivo.setPath(filePath + ".output.txt");
 		
 		ArrayList<Consulta> consultas = null;
